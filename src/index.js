810a0d5844fbc1ad15418831881b2f6af8573bec
Veronica <tveronichka@gmail.com>
2021-11-18 19:08:01 +0300

    2nd lesson

--- src/index.js
+++ src/index.js
@@ -1,25 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { useState } from "react";
 
-const message = <p>"Test message"</p>;
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [value, setValue] = useState("");
+  const ref = useRef(null);
 
-const Message = (props) => {
+  useEffect(() => {
+    const lastMessages = messages[messages.length - 1];
+    let timerId = null;
+    if (messages.length && lastMessages.author !== "Bot") {
+      timerId = setTimeout(() => {
+        setMessages([
+          ...messages,
+          { author: "Bot", message: "Hello from bot" },
+        ]);
+      }, 500);
+    }
+    return () => clearInterval(timerId);
+  }, [messages]);
+
+  useEffect(() => {
+    ref.current?.focus();
+  }, []);
+
+  // const handleChangeValue = (e) => setValue(e.target.value);
+  const sendMessage = () => {
+    setMessages([...messages, { author: "User", message: value }]);
+    setValue("");
+  };
+
   return (
-    <div className="message">
-      <h1>Message</h1>
-      <p>Message: {message}</p>
+    <div>
+      {messages.map((message) => (
+        <div>{message.message}</div>
+      ))}
+
+      <input
+        ref={ref}
+        placeholder="enter your message"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={sendMessage}>send message</button>
     </div>
   );
 };
 
-function App() {
-  return <Message />;
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <App newMessage={message} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
